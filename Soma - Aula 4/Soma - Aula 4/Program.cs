0804ebb2560d3a247778aebde762ea7a0b58bde9
alencartha <alencar_tha@hotmail.com>
2022-02-05 19:23:39 -0300

    first commit

---
+++ Soma - Aula 4/Soma - Aula 4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soma___Aula_4
+{
+    internal class Program
+    {
+        int n1;
+        int n2;
+        int soma;
+
+        static void Main(string[] args)
+        {
+            int n1;
+            int n2;
+            int soma;
+
+            Console.Write("Digite o primeiro número: ");
+            n1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Digite o segundo número: ");
+            n2 = Convert.ToInt32(Console.ReadLine());
+
+            soma = Soma(n1, n2);
+            Console.Write("A soma entre {0} e {1} é {2}", n1, n2, soma);
+            Console.ReadLine();
+        }
+
+        public static int Soma(int n1, int n2)
+        {
+            return n1 + n2;
+        }
+    }
+}
---
+++ Strings - Aula1/Strings - Aula1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings___Aula1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string nome;
+            int idade;
+            int dias;
+
+            Console.Write("Digite seu nome: ");
+            nome = Console.ReadLine();
+
+            Console.Write("Digite sua idade: ");
+            idade = Convert.ToInt32(Console.ReadLine());
+
+            dias = idade * 365;
+
+            Console.Write("Olá {0}, sua idade é {1} e você já viveu {2} dias", nome, idade, dias);
+            Console.ReadLine();
+
+        }
+    }
+}
