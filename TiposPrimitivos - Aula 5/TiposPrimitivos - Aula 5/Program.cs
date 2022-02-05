b8fe6ce8439cd2c98c62444794a6437abe702bea
alencartha <alencar_tha@hotmail.com>
2022-02-05 20:40:52 -0300

    tipos primitivos

---
+++ TiposPrimitivos - Aula 5/TiposPrimitivos - Aula 5/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+
+namespace TiposPrimitivos___Aula_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Tipos Primitivos ");
+
+            string nome = "José";
+            short numeroMenor = 100;
+            int numero = 20;
+            long numeroGrande = 125014851515;
+
+            double commissao = 32.45;
+            decimal valor = Convert.ToDecimal(1000.56);
+            decimal valor1 = 100.55m;
+
+            DateTime data = DateTime.Now;
+            DateTime dataExemplo = new DateTime(2022, 12, 11);
+
+
+            //formatar data
+            Console.WriteLine("{0:d}", dataExemplo);
+
+
+            //data por extenso
+            Console.WriteLine("{0:D}", dataExemplo);
+
+
+            //dia da semana por extenso
+            Console.WriteLine("{0:ddddddd}", dataExemplo);
+
+
+            //formatar em moeda
+            Console.WriteLine("{0:c}", valor);
+
+
+            //data hoje
+            Console.WriteLine("{0:d}", data);
+
+
+            Console.ReadLine();
+
+        
+        }
+    }
+}
