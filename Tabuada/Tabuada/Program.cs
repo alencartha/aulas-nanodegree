08a12df4e059adf72b8b2cfbaeaa4bcbb918f37a
alencartha <alencar_tha@hotmail.com>
2022-03-12 13:44:06 -0300

    tabuada atividade

---
+++ Tabuada/Tabuada/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tabuada
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            int numero;
+
+            Console.Write("Digite um número para multiplicação: ");
+            numero = Convert.ToInt16(Console.ReadLine());
+
+            for (int i =1; i <= 10; i++)
+            {
+                int multiplicacao = numero * i;
+                Console.WriteLine("{0} x {1} = {2} ", numero, i, multiplicacao);
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
